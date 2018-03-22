7c9dedf0ae54d703139b977a13622fb4b092667c
tangdayi <http://tdy@stash.wens.com.cn>
2018-03-22 22:52:05 +0800

    develop: 完成所有的基础模块：用户反馈，用餐，供应商，用户，公司

---
+++ app/dy_base/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'FBB DYBASE',
+    'sequence': 2,
+    'author': 'YiDaTang',
+    'version': '1.0',
+    'depends': ['base', 'web', 'decimal_precision'],
+    'category': 'DYBASE',
+    'summary': '基础数据管理',
+    'description': """ 基础数据管理初始化 """,
+    'data': [
+        'security/ir.model.access.csv',
+        'sequences.xml',
+        'views/store_type_view.xml',
+        'views/restaurant_view.xml',
+        'views/address_view.xml',
+        'views/department_view.xml',
+        'views/company_view.xml',
+        'views/score_view.xml',
+        'statics/init_dy_province_data.xml',
+        'views/menus.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+
+}
